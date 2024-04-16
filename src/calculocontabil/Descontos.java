0c02b8dc9bc7d85956e4eeb5b9d52007591c537c
Binnyzin <Binnyzin@192.168.10.16>
2024-04-16 16:32:39 -0300

    Finalização método INSS

--- src/calculocontabil/Descontos.java
+++ src/calculocontabil/Descontos.java
@@ -1,15 +1,31 @@
 package calculocontabil;
 
 public class Descontos {
-	float IRPF, VT, VR, convenioMedico;
-	double INSS;
+	public static double IRPF, INSS, VT, VR, convenioMedico, VA;
 
-	public static double descontoINSS(double salario){ //Calcular o IRPF precisa dos ganhos somados, descontar o INSS antes
-		if(salario <= 1412) { //Descontando o INSS
+	public static double descontoINSS(double salario){
+		if(salario <= 1412) {
 			salario -= salario*0.075;
 			return salario;
-		} else{
-			return 0;
-		}
+			}
+			if(salario > 1412 && salario <= 2666.68){
+				salario -= salario*0.09;
+				return salario;
+			} if(salario > 2666.68&& salario <= 4000.03){
+				salario -= salario*0.12;
+				return salario;
+			} if(salario > 4000.03 && salario <= 7786.02){
+				salario -= salario*0.14;
+				return salario;
+			} if(salario > 7786.02){
+				salario -= 7786.02*0.14;
+				return salario;
+			} else {
+				return salario;
+			}
+	}
+	
+	public static double descontoIRPF(double ganhos){
+		
 	}
 }
