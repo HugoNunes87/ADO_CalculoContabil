659ecba7d1395919e4aef8c4deb1888065f8026d
Binnyzin <Binnyzin@192.168.10.16>
2024-04-17 15:39:14 -0300

    Finalziado métodos descontos (VR, Convenio e VA)

--- src/calculocontabil/Descontos.java
+++ src/calculocontabil/Descontos.java
@@ -3,7 +3,7 @@ package calculocontabil;
 public class Descontos {
 	public static double IRPF, INSS, VT, VR, convenioMedico, VA;
 
-	public static double descontoINSS(double salario){
+	public static double INSS(double salario){
 		if(salario <= 1412) {
 			INSS = (salario*0.075);
 			} else if(salario > 1412 && salario <= 2666.68){
@@ -24,9 +24,9 @@ public class Descontos {
 			return INSS;
 	}
 	
-	public static double descontoIRPF(double salario, int dependentes){
+	public static double IRPF(double salario, int dependentes){
 		
-		descontoINSS(salario);
+		INSS(salario);
 		
 		double descontoDependentes = dependentes*189.59;
 		double descontosTotais = INSS + descontoDependentes;
@@ -34,12 +34,16 @@ public class Descontos {
 		
 		if(salarioFinalIRPF < 2112){
 			IRPF = 0;
+			
 		} else if(salarioFinalIRPF > 2112 && salarioFinalIRPF <= 2826.65){
 			IRPF = (salarioFinalIRPF*0.075)-158.40;
+			
 		} else if(salarioFinalIRPF > 2826.65 && salarioFinalIRPF <= 3751.05){
 			IRPF = (salarioFinalIRPF*0.15)-370.4;
+			
 		} else if(salarioFinalIRPF > 3751.06 && salarioFinalIRPF <= 4664.68){
 			IRPF = (salarioFinalIRPF*0.225)-651.73;
+			
 		} else {
 			IRPF = (salarioFinalIRPF*0.275)-884.96;
 		}
