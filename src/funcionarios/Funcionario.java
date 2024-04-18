8e9553fba832f9d1f4c6fb3d428d58df818aa9a3
Binnyzin <Binnyzin@192.168.10.2>
2024-04-18 14:53:35 -0300

    Atualzação Classe funcionario e Holerite

--- src/funcionarios/Funcionario.java
+++ src/funcionarios/Funcionario.java
@@ -1,4 +1,5 @@
 package funcionarios;
+import java.util.Scanner;
 
 public class Funcionario {
 	public String nomeFuncionario;
@@ -6,6 +7,21 @@ public class Funcionario {
 	
 	public String GetNome(){
 		return nomeFuncionario;
+	}
+	
+	public static void CadastroFuncionario(){
+		Scanner reader = new Scanner(System.in);
+		System.out.println(" _______________________________________________________________");
+		System.out.println("|\t\t\t\t\t\t\t\t|");
+		System.out.println("|\t\t\t Bem vindo ao Holeri+ \t\t\t|");
+		System.out.println("|_______________________________________________________________|");
+		System.out.printf("\tDigite quantos funcionarios você deseja cadastrar: ");
+		int qtdFuncionarios = reader.nextInt();
+		
+		for(int i = 0; i < qtdFuncionarios; i++){
+			Funcionario f1 = new Funcionario();
+			System.out.printf("|\t\t     Cadastro do %dº funcionario\t\t        |\n", i+1);
+		}
 	}
 	
 }
--- src/impressaoholerite/ImpressaoHolerite.java
+++ src/impressaoholerite/ImpressaoHolerite.java
@@ -1,10 +1,13 @@
 package impressaoholerite;
 
 import calculocontabil.Descontos;
+import calculocontabil.Ganhos;
+import funcionarios.Funcionario;
+import java.util.Scanner;
 
 public class ImpressaoHolerite {
 
 	public static void main(String[] args) {
-		
+		Funcionario.CadastroFuncionario();
 	}
 }
